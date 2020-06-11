f6467401685a4155f0d7685fb166be66c5bf385f
isabella g henriques <isabellagh88@gmail.com>
2020-06-11 21:29:27 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,10 @@
+#This file will start our application. This file will require the environment file we created earlier to provide our app with access to our gems.
+
+require_relative '../config/environment'
+
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
+
